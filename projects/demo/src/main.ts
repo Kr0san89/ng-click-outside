c006924f5393d5a5322090b301218fa47cf24654
Manuel Schmidt <ms@manuel-schmidt.at>
2024-12-14 19:36:52 +0100

    chore: remove ssr

--- projects/demo/src/main.ts
+++ projects/demo/src/main.ts
@@ -1,26 +1,16 @@
-import {APP_ID, enableProdMode, importProvidersFrom} from '@angular/core';
+import {enableProdMode} from '@angular/core';
 
 import {environment} from './environments/environment';
-import {bootstrapApplication, BrowserModule} from '@angular/platform-browser';
-import {
-  NgClickOutsideAttachOutsideDirective,
-  NgClickOutsideDirective,
-  NgClickOutsideEmitOnBlurDirective,
-  NgClickOutsideExcludeDirective
-} from 'ng-click-outside2';
+import {bootstrapApplication} from '@angular/platform-browser';
 import {AppComponent} from './app/app.component';
+import {appConfig} from "./app/app.config";
 
 if (environment.production) {
   enableProdMode();
 }
 
 function bootstrap() {
-  bootstrapApplication(AppComponent, {
-    providers: [
-      importProvidersFrom(BrowserModule, NgClickOutsideDirective, NgClickOutsideAttachOutsideDirective, NgClickOutsideExcludeDirective, NgClickOutsideEmitOnBlurDirective),
-      {provide: APP_ID, useValue: 'serverApp'}
-    ]
-  })
+  bootstrapApplication(AppComponent, appConfig)
     .catch(err => console.error(err));
 }
 
